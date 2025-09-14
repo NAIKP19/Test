9038b57bd6bff77168783c0e8dfa31350d69c1f8
NAIKP19 <pramod.naik@pfizer.com>
2025-09-14 22:04:43 +0530

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,832 @@
+[
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-14",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-15",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-16",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-17",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-18",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-19",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-20",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-21",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-22",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-23",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-24",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-25",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-26",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-27",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-28",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-29",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-09-30",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-01",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-02",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-03",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-04",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-05",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-06",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-07",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-08",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-09",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-10",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-11",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-12",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-13",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-14",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-15",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-16",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-17",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-18",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-19",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-20",
+        "ARGENTINA": 0
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-21",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-22",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-23",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-24",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-25",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-26",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-27",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 1,
+        "date": "2025-10-28",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-10-29",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-10-30",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-10-31",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-01",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-02",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-03",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-04",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-05",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-06",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-07",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-08",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-09",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-10",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-11",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-12",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-13",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-14",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-15",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-16",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-17",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-18",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-19",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-20",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-21",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-22",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-23",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-24",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-25",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-26",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-27",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 2,
+        "date": "2025-11-28",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-11-29",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-11-30",
+        "ARGENTINA": 1
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-01",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-02",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-03",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-04",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-05",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-06",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-07",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-08",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-09",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-10",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-11",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-12",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-13",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-14",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-15",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-16",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-17",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-18",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-19",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-20",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-21",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-22",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-23",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-24",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-25",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-26",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-27",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-28",
+        "ARGENTINA": 2
+    },
+    {
+        "AUSTRALIA": 3,
+        "date": "2025-12-29",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2025-12-30",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2025-12-31",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-01",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-02",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-03",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-04",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-05",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-06",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-07",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-08",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-09",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-10",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-11",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-12",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-13",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-14",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-15",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-16",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-17",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-18",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-19",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-20",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-21",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-22",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-23",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-24",
+        "ARGENTINA": 3
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-25",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-26",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-27",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-28",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-29",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-30",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-01-31",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-02-01",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 4,
+        "date": "2026-02-02",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-03",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-04",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-05",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-06",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-07",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-08",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-09",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-10",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-11",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-12",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-13",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-14",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-15",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-16",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-17",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-18",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-19",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-20",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-21",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-22",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-23",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-24",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-25",
+        "ARGENTINA": 4
+    },
+    {
+        "AUSTRALIA": 5,
+        "date": "2026-02-26",
+        "ARGENTINA": 5
+    }
+]
